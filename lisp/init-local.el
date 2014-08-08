16827c1e4f90c967060888bcba9e48e559099a9a
qinshulei <527072230@qq.com>
2014-08-08 16:13:24 +0800

    init

---
+++ lisp/init-local.el
@@ -0,0 +1,37 @@
+;;; install emmet
+(require-package 'emmet-mode)
+(require 'emmet-mode)
+(add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
+(add-hook 'html-mode-hook 'emmet-mode)
+(add-hook 'css-mode-hook  'emmet-mode)
+
+
+;;; set theme
+(dark)
+
+
+;;;insert time function
+(defun insert-date ()
+  "Insert current date yyyy-mm-dd."
+  (interactive)
+  (when (region-active-p)
+        (delete-region (region-beginning) (region-end) )
+        )
+  (insert (format-time-string "%Y-%m-%d %H:%M:%S"))
+  )
+
+;;binder insert date to key c-x t
+(global-set-key (kbd "C-x t") 'insert-date)
+
+
+;;;search key bing
+(defun search-keybind (regexp &optional nlines)
+  (interactive (occur-read-primary-args))
+  (save-excursion
+    (describe-bindings)
+    (set-buffer "*Help*")
+    (occur regexp nlines)
+    (delete-windows-on "*Help*")))
+
+
+(provide 'init-local)
