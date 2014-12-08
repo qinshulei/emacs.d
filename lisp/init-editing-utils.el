4ab11e865d0d149a69a3bca3a82548f59423e9fc
qinshulei <527072230@qq.com>
2014-12-08 10:18:30 +0800

    add cheat.el

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -329,7 +329,7 @@ With arg N, insert N newlines."
 
 
 (require-package 'guide-key)
-(setq guide-key/guide-key-sequence '("C-x r" "C-x 4" "C-x 5" "C-c ;" "C-c ; f" "C-c ' f" "C-x n"))
+(setq guide-key/guide-key-sequence '("C-x r" "C-x 4" "C-x 5" "C-c ;" "C-c ; f" "C-c ' f" "C-x n" "C-c p" "C-c '"))
 (guide-key-mode 1)
 (diminish 'guide-key-mode)
 
