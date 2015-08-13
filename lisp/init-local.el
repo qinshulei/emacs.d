9a7bf84a5ad8881700b5300318bd06caeaccc258
qinshulei <527072230@qq.com>
2015-08-13 11:26:57 +0800

    set gutter keybind

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -501,6 +501,14 @@
 (global-git-gutter-mode +1)
 ;; (add-hook 'ruby-mode-hook 'git-gutter-mode)
 ;; (add-hook 'python-mode-hook 'git-gutter-mode)
-
+;; (global-set-key (kbd "C-x C-g") 'git-gutter:toggle)
+;; (global-set-key (kbd "C-x v =") 'git-gutter:popup-hunk)
+;; Jump to next/previous hunk
+(global-set-key (kbd "C-x p") 'git-gutter:previous-hunk)
+(global-set-key (kbd "C-x n") 'git-gutter:next-hunk)
+;; Stage current hunk
+;; (global-set-key (kbd "C-x v s") 'git-gutter:stage-hunk)
+;; Revert current hunk
+;; (global-set-key (kbd "C-x v r") 'git-gutter:revert-hunk)
 
 (provide 'init-local)
